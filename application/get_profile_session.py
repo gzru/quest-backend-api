0955ac753ae880a528c8277c589cf5eee9c6784d
Dmitry Vasilyev <d.vasilyev@aiarlabs.com>
2017-10-11 17:21:40 +0000

    small fixes

--- application/get_profile_session.py
+++ application/get_profile_session.py
@@ -44,14 +44,19 @@ class GetProfileSession(ProfileSessionBase):
             result['meta_blob'] =  self._get_meta(self._query.user_id)
 
         if 'friends' in self._query.properties:
-            #result['friends'] =  self._get_friends(self._query.user_id)
-            result['friends'] =  self._get_friends_fb(result['facebook_user_id'], result['facebook_access_token'])
+            facebook_user_id = result['facebook_user_id']
+            facebook_access_token = result['facebook_access_token']
+            if facebook_user_id != None and facebook_access_token != None:
+                result['friends'] = self._get_friends_fb(facebook_user_id, facebook_access_token)
+            else:
+                result['friends'] = list()
 
         return json.dumps(result)
 
     def _get_friends_fb(self, facebook_user_id, facebook_access_token):
-        friends = list()
+        logging.info('_get_friends_fb: Retrieve friends from facebook, facebook_user_id = {}'.format(facebook_user_id))
 
+        friends = list()
         next_page_uri = 'https://graph.facebook.com/{}/friends?access_token={}'.format(facebook_user_id, facebook_access_token)
         while next_page_uri:
             try:
@@ -61,7 +66,6 @@ class GetProfileSession(ProfileSessionBase):
                 # take profiles
                 data = parsed.get('data')
                 for entry in data:
-                    print entry
                     facebook_user_id = entry.get('id')
 
                     profile = dict()
@@ -80,11 +84,13 @@ class GetProfileSession(ProfileSessionBase):
             except Exception as ex:
                 logging.error(ex)
                 raise
+
+        logging.info('_get_friends_fb: Got {} entries'.format(len(friends)))
         return friends
 
     def _get_user_id_by_fb(self, facebook_user_id):
         user_id = self._get_external_link(facebook_user_id)
-        if not user_id:
+        if user_id == None:
             logging.warning('Can\'t find user by facebook user id')
 
             info = UserInfo()
