f606b3a2c7f8972eecddbbd5a6b969b5519b0a2e
Dmitry Vasilyev <d.vasilyev@aiarlabs.com>
2017-10-19 07:57:51 +0000

    get_friends_session: user_id is optional + twilio channel

--- application/get_friends_session.py
+++ application/get_friends_session.py
@@ -7,13 +7,17 @@ import logging
 class GetFriendsQuery(Query):
 
     def __init__(self):
+        self.user_token = None
         self.user_id = None
         self.limit = None
         self.cursor = None
 
     def parse(self, data):
         tree = self._parse_json(data)
-        self.user_id = self._get_required_int64(tree, 'user_id')
+        self.user_token = self._get_required_str(tree, 'user_token')
+        self.user_id = self._get_optional_int64(tree, 'user_id')
+        if self.user_id == None:
+            self.user_id = int(self.user_token)
 
         self.limit = self._get_optional_int64(tree, 'limit')
         if self.limit == None:
@@ -32,10 +36,14 @@ class GetFriendsSession(ProfileSessionBase):
 
     def execute(self):
         page = self._engine.get_friends(self._query.user_id, self._query.limit, self._query.cursor)
+        relations = self._engine.get_relations_many(self._query.user_id, page.data)
 
         friends = list()
         # Retrieve aux data
-        for user_id in page.data:
+        for i, user_id in enumerate(page.data):
+            if not relations[i].is_friends:
+                continue
+
             profile = dict()
             # fix me, use group request
             info = None
@@ -49,10 +57,12 @@ class GetFriendsSession(ProfileSessionBase):
 
             profile['user_id'] = user_id
             profile['name'] = info.get('name')
+            profile['twilio_channel'] = relations[i].twilio_channel
 
             friends.append(profile)
 
         result = {
+            'success': True,
             'data': friends,
             'paging': { 'cursor': page.cursor_code, 'has_next': page.has_next }
         }
