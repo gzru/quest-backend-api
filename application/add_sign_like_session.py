196e8180f3bfba793213ac1fb41067b0436f3d2e
Dmitry Vasilyev <d.vasilyev@aiarlabs.com>
2017-10-19 08:13:10 +0000

    add views/likes sessions

---
+++ application/add_sign_like_session.py
@@ -0,0 +1,46 @@
+from query import Query
+from signs_engine import SignsEngine
+from users_engine import UsersEngine
+import json
+
+
+class AddSignLikeQuery(Query):
+
+    def __init__(self):
+        self.user_id = None
+        self.sign_id = None
+
+    def parse(self, data):
+        tree = self._parse_json(data)
+        self.user_id = self._get_required_int64(tree, 'user_id')
+        self.sign_id = self._get_required_int64(tree, 'sign_id')
+
+
+class AddSignLikeSession(object):
+
+    def __init__(self, global_context):
+        self._signs_engine = SignsEngine(global_context)
+        self._users_engine = UsersEngine(global_context)
+
+    def parse_query(self, data):
+        self._query = AddSignLikeQuery()
+        self._query.parse(data)
+
+    def execute(self):
+        self._signs_engine.add_sign_like(self._query.user_id, self._query.sign_id)
+        self._users_engine.put_like(self._query.user_id, self._query.sign_id)
+
+        return json.dumps({'success': True})
+
+
+if __name__ == "__main__":
+    from global_context import GlobalContext
+    global_context = GlobalContext()
+    global_context.initialize()
+
+    s = AddSignLikeSession(global_context)
+    s.parse_query('{"user_id":123, "sign_id":345}')
+    print s.execute()
+
+
+
