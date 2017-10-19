65a441b6f952922c3ceb6f23bf44fcf52a01bdfb
Dmitry Vasilyev <d.vasilyev@aiarlabs.com>
2017-10-19 08:01:10 +0000

    make_messaging_token_session: use twilio connector

--- application/make_messaging_token_session.py
+++ application/make_messaging_token_session.py
@@ -1,7 +1,4 @@
 from query import Query
-from twilio.jwt.access_token import AccessToken
-from twilio.jwt.access_token.grants import ChatGrant
-import settings
 import json
 
 
@@ -17,28 +14,22 @@ class MakeMessagingTokenQuery(Query):
         self.sign_id = self._get_required_str(tree, 'uuid')
 
 
-class MakeMessagingTokenSession:
+class MakeMessagingTokenSession(object):
 
     def __init__(self, global_context):
-        self._account_sid = settings.TWILIO_ACCOUNT_SID
-        self._api_key = settings.TWILIO_API_KEY
-        self._api_secret = settings.TWILIO_API_SECRET
-        self._chat_service_sid = settings.TWILIO_CHAT_SERVICE_SID
+        self._twilio_connector = global_context.twilio_connector
 
     def parse_query(self, data):
         self._query = MakeMessagingTokenQuery()
         self._query.parse(data)
 
     def execute(self):
-        identity = '{}_{}'.format(self._query.user_id, self._query.uuid)
-
-        # Create access token with credentials
-        token = AccessToken(self._account_sid, self._api_key, self._api_secret, identity=identity)
-
-        # Create an Chat grant and add to token
-        chat_grant = ChatGrant(service_sid=self._chat_service_sid)
-        token.add_grant(chat_grant)
-
-        result = { 'token': token.to_jwt().decode('utf-8') }
+        token = self._twilio_connector.make_messaging_token(self._query.user_id, self._query.uuid)
+        result = {
+            'success': True,
+            'data': {
+                'token': token
+            }
+        }
         return json.dumps(result)
 
