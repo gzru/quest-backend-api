fdfdd0c26f090df1b19b0e38480a2c0eebab06b8
Dmitry Vasilyev <d.vasilyev@aiarlabs.com>
2017-10-23 16:26:24 +0000

    add user properties

--- application/update_profile_session.py
+++ application/update_profile_session.py
@@ -6,21 +6,20 @@ class UpdateProfileQuery(ProfileQueryBase):
 
     def __init__(self):
         self.user_id = None
-        self.facebook_user_id = None
-        self.facebook_access_token = None
         self.name = None
-        self.friends = None
-        self.meta_blob = None
+        self.username = None
         self.email = None
+        self.meta_blob = None
+        self.picture_blob = None
 
     def parse(self, data):
         tree = self._parse_json(data)
         self.user_id = int(self._get_required_str(tree, 'user_token'))
-        self.facebook_user_id = self._get_optional_str(tree, 'facebook_user_id')
-        self.facebook_access_token = self._get_optional_str(tree, 'facebook_access_token')
         self.name = self._get_optional_str(tree, 'name')
-        self.meta_blob = self._get_optional_blob(tree, 'meta_blob')
+        self.username = self._get_optional_str(tree, 'username')
         self.email = self._get_optional_str(tree, 'email')
+        self.meta_blob = self._get_optional_blob(tree, 'meta_blob')
+        self.picture_blob = self._get_optional_blob(tree, 'picture_blob')
 
 
 class UpdateProfileSession(ProfileSessionBase):
@@ -31,13 +30,32 @@ class UpdateProfileSession(ProfileSessionBase):
 
     def execute(self):
         user_id = self._query.user_id
-        if not self._check_exists(user_id):
+        user_info = self._engine.get_info(user_id)
+        if user_info == None:
             raise Exception('User not found')
 
-        self._put_meta(user_id, self._query)
-        self._put_friends(user_id, self._query)
-        self._put_external_link(user_id, self._query.facebook_user_id)
-        self._put_info(user_id, self._query)
+        info_updated = False
+        if self._query.name != None: user_info.name = self._query.name; info_updated = True
+        if self._query.username != None: user_info.username = self._query.username; info_updated = True
+        if self._query.email != None: user_info.email = self._query.email; info_updated = True
+        if info_updated:
+            self._engine.put_info(user_info)
 
-        return json.dumps({ 'user_token': str(user_id) })
+        if self._query.meta_blob:
+            self._put_meta(user_id, self._query)
+
+        if self._query.picture_blob != None:
+            self._engine.put_picture(user_id, self._query.picture_blob)
+
+        return json.dumps({'success': True, 'user_token': str(user_id)})
+
+
+if __name__ == "__main__":
+    from global_context import GlobalContext
+    global_context = GlobalContext()
+    global_context.initialize()
+
+    s = UpdateProfileSession(global_context)
+    s.parse_query('{"user_token": "6823619285704494896", "name": "Ivan Ivanov"}')
+    print s.execute()
 
